11600a6b22aa5ba478dae2a7609dc429eef243f3
Sacha Delanoue <contact@shaac.me>
2016-04-03 21:21:47 +0200

    License code under GPLv3+

--- src/action_detruire.cc
+++ src/action_detruire.cc
@@ -1,3 +1,22 @@
+/*
+** This file is part of Prologin2016, a rules library for stechec2.
+**
+** Copyright (c) 2016 Association Prologin <info@prologin.org>
+**
+** Prologin2016 is free software: you can redistribute it and/or modify
+** it under the terms of the GNU General Public License as published by
+** the Free Software Foundation, either version 3 of the License, or
+** (at your option) any later version.
+**
+** Prologin2016 is distributed in the hope that it will be useful,
+** but WITHOUT ANY WARRANTY; without even the implied warranty of
+** MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+** GNU General Public License for more details.
+**
+** You should have received a copy of the GNU General Public License
+** along with Prologin2016.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
 #include "actions.hh"
 
 int ActionDetruire::check(const GameState* st) const
