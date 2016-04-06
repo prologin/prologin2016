22f9611127f77d9db149de6c1849f95bc4a27cf5
cbeausei <cbeausei@debian>
2016-04-06 21:49:56 +0200

    map editor modified

--- maps/map_editor.py
+++ maps/map_editor.py
@@ -13,9 +13,11 @@ larg = 600
 # Constantes
 N = 39
 nombre_pulsars = N // 3
+couleur_pulsars = ['black', 'red', 'yellow', 'blue', 'green']
 
 # Grille
 grille = [["none" for j in range(N)] for i in range(N)]
+type_pulsars = [[0 for j in range(N)] for i in range(N)]
 
 # Positionnement du plateau dans la fenêtre
 delta = 30
@@ -29,12 +31,12 @@ xdelta = (xbas - xhaut) / N
 ydelta = (ybas - yhaut) / N
 
 # Variables de jeu
-case_selectionnee = (-1, -1)
 fichier = 'map_default'
+types = [(40, 85, 25), (20, 55, 10), (10, 100, 4), (6, 40, 5), (3, 100, 1)]
 
 
 def click_gauche(event):  # Définit ce qu'il se passe lors de l'appui sur le click gauche
-    global case_selectionnee
+    case_selectionnee = (-1, -1)
     x, y = event.x, event.y
     for i in range(N):
         for j in range(N):
@@ -42,19 +44,39 @@ def click_gauche(event):  # Définit ce qu'il se passe lors de l'appui sur le cl
             xmax, ymax = xhaut + (i + 1) * xdelta, yhaut + (j + 1) * ydelta
             if xmin <= x <= xmax and ymin <= y <= ymax:
                 case_selectionnee = (i, j)
-    rafraichir()
-
-
-def appui_clavier(event):
-    global case_selectionnee, grille
-    lettre = event.char
-    i, j = case_selectionnee
-    if lettre in ['\r', '\n']:
+    if case_selectionnee != (-1, -1):
+        i, j = case_selectionnee
         if 0 < i < N-1 and 0 < j < N-1:
             if grille[i][j] == 'pulsar':
-                grille[i][j] = 'none'
+                type_p = 0
+                if a_v.get() == 1:
+                    type_p = 0
+                if b_v.get() == 1:
+                    type_p = 1
+                if c_v.get() == 1:
+                    type_p = 2
+                if d_v.get() == 1:
+                    type_p = 3
+                if e_v.get() == 1:
+                    type_p = 4
+                if type_p == type_pulsars[i][j]:
+                    grille[i][j] = 'none'
+                else:
+                    type_pulsars[i][j] = type_p
             else:
                 grille[i][j] = 'pulsar'
+                type_p = 0
+                if a_v.get() == 1:
+                    type_p = 0
+                if b_v.get() == 1:
+                    type_p = 1
+                if c_v.get() == 1:
+                    type_p = 2
+                if d_v.get() == 1:
+                    type_p = 3
+                if e_v.get() == 1:
+                    type_p = 4
+                type_pulsars[i][j] = type_p
         elif i + j in [0, N - 1, 2 * N - 2]:
             return
         elif i in [0, N - 1]:
@@ -67,26 +89,11 @@ def appui_clavier(event):
                 grille[i][j] = 'none'
             else:
                 grille[i][j] = 'base_2'
-    if lettre == 'q' and i > 0:
-        case_selectionnee = (i - 1, j)
-    if lettre == 'd' and i < N - 1:
-        case_selectionnee = (i + 1, j)
-    if lettre == 'z' and j > 0:
-        case_selectionnee = (i, j - 1)
-    if lettre == 's' and j < N - 1:
-        case_selectionnee = (i, j + 1)
     rafraichir()
 
 
 def remplir_case(i, j, couleur):
     cadre.create_polygon(xhaut + i * xdelta, yhaut + j * ydelta, xhaut + (i + 1) * xdelta, yhaut + j * ydelta, xhaut + (i + 1) * xdelta, yhaut + (j + 1) * ydelta, xhaut + i * xdelta, yhaut + (j + 1) * ydelta, fill=couleur, outline="black", width=0.5)
-
-
-def dessiner_selection(i, j):
-    cadre.create_polygon(xhaut + i * xdelta, yhaut + j * ydelta, xhaut + (i + 1) * xdelta, yhaut + j * ydelta, outline="red", width=3.0)
-    cadre.create_polygon(xhaut + (i + 1) * xdelta, yhaut + j * ydelta, xhaut + (i + 1) * xdelta, yhaut + (j + 1) * ydelta, outline="red", width=3.0)
-    cadre.create_polygon(xhaut + (i + 1) * xdelta, yhaut + (j + 1) * ydelta, xhaut + i * xdelta, yhaut + (j + 1) * ydelta, outline="red", width=3.0)
-    cadre.create_polygon(xhaut + i * xdelta, yhaut + (j + 1) * ydelta, xhaut + i * xdelta, yhaut + j * ydelta, outline="red", width=3.0)
 
 
 def rafraichir():
@@ -103,15 +110,43 @@ def rafraichir():
             if grille[i][j] == "base_2":
                 remplir_case(i, j, "#5b3c11")
             if grille[i][j] == "pulsar":
-                remplir_case(i, j, "#000000")
-    a, b = case_selectionnee
-    if 0 <= a <= N and 0 <= b <= N:
-        dessiner_selection(a, b)
+                remplir_case(i, j, couleur_pulsars[type_pulsars[i][j]])
+    a_l1.delete('@0,0', END)
+    a_l2.delete('@0,0', END)
+    a_l3.delete('@0,0', END)
+    b_l1.delete('@0,0', END)
+    b_l2.delete('@0,0', END)
+    b_l3.delete('@0,0', END)
+    c_l1.delete('@0,0', END)
+    c_l2.delete('@0,0', END)
+    c_l3.delete('@0,0', END)
+    d_l1.delete('@0,0', END)
+    d_l2.delete('@0,0', END)
+    d_l3.delete('@0,0', END)
+    e_l1.delete('@0,0', END)
+    e_l2.delete('@0,0', END)
+    e_l3.delete('@0,0', END)
+    a_l1.insert('@0,0', types[0][0])
+    a_l2.insert('@0,0', types[0][1])
+    a_l3.insert('@0,0', types[0][2])
+    b_l1.insert('@0,0', types[1][0])
+    b_l2.insert('@0,0', types[1][1])
+    b_l3.insert('@0,0', types[1][2])
+    c_l1.insert('@0,0', types[2][0])
+    c_l2.insert('@0,0', types[2][1])
+    c_l3.insert('@0,0', types[2][2])
+    d_l1.insert('@0,0', types[3][0])
+    d_l2.insert('@0,0', types[3][1])
+    d_l3.insert('@0,0', types[3][2])
+    e_l1.insert('@0,0', types[4][0])
+    e_l2.insert('@0,0', types[4][1])
+    e_l3.insert('@0,0', types[4][2])
 
 
 def initialiser():
     global grille, delta, xbas, ybas, xhaut, yhaut, longueur, largeur, xdelta, ydelta, nombre_pulsars
     grille = [["none" for j in range(N)] for i in range(N)]
+    type_pulsars = [[0 for j in range(N)] for i in range(N)]
     for i in range(N // 3, 2 * N // 3):
         grille[0][i] = "base_1"
         grille[N - 1][i] = "base_1"
@@ -127,6 +162,11 @@ def initialiser():
     xdelta = (xbas - xhaut) / N
     ydelta = (ybas - yhaut) / N
     nombre_pulsars = N // 3
+    a_v.set(1)
+    b_v.set(0)
+    c_v.set(0)
+    d_v.set(0)
+    e_v.set(0)
 
 
 def free(i, j):
@@ -139,8 +179,10 @@ def creation_pulsars():
     for k in range(nombre_pulsars):
         i = randint(N // 4, 3 * N // 4)
         j = randint(N // 4, 3 * N // 4)
+        type_p = randint(0, 4)
         if free(i - 1, j) and free(i + 1, j) and free(i, j - 1) and free(i, j + 1):
             grille[i][j] = "pulsar"
+            type_pulsars[i][j] = type_p
 
 
 def gene():
@@ -148,6 +190,13 @@ def gene():
     N = int(text_n.get('@0,0', END))
     initialiser()
     creation_pulsars()
+    rafraichir()
+
+
+def clear():
+    global N
+    N = int(text_n.get('@0,0', END))
+    initialiser()
     rafraichir()
 
 
@@ -163,6 +212,18 @@ def start():  # Démarrage de la partie
 
 def save():
     s = str(N) + '\n'
+    nb = 0
+    for i in range(N):
+        for j in range(N):
+            if grille[i][j] == 'pulsar':
+                nb += 1
+    s += str(nb) + '\n'
+    for j in range(N):
+        for i in range(N):
+            if grille[i][j] == 'pulsar':
+                type_p = type_pulsars[i][j]
+                a, b, c = types[type_p]
+                s += str(a) + ' ' + str(b) + ' ' + str(c) + '\n'
     for j in range(N):
         for i in range(N):
             if grille[i][j] in ['base_1', 'base_2']:
@@ -185,32 +246,115 @@ def load():
     s = f.readlines()
     f.close()
     N = int(s[0])
+    nb = int(s[1])
     text_n.delete('@0,0', END)
     text_n.insert(INSERT, str(N))
     initialiser()
     for i in range(N):
         for j in range(N):
-            if s[j + 1][i] == '#':
+            if s[j + nb + 2][i] == '#':
                 grille[i][j] = 'pulsar'
-            if s[j + 1][i] == '.':
+            if s[j + nb + 2][i] == '.':
                 grille[i][j] = 'none'
-            elif i + j not in [-1, N - 2, 2 * N - 3] and s[j + 1][i] == 'B':
+            elif i + j not in [0, N - 1, 2 * N - 2] and s[j + nb + 2][i] == 'B':
                 if i in [0, N - 1]:
                     grille[i][j] = 'base_1'
                 else:
                     grille[i][j] = 'base_2'
+    for i in range(5):
+        a, b, c = s[i + 2 + nb + N].split('\n')[0].split()
+        types[i] = (a, b, c)
+    k = 0
+    for j in range(N):
+        for i in range(N):
+            if s[j + nb + 2][i] == '#':
+                a, b, c = s[k + 2].split('\n')[0].split()
+                for type_p in range(5):
+                    if types[type_p] == (a, b, c):
+                        type_pulsars[i][j] = type_p
+                        print(type_p)
+                k += 1
     rafraichir()
+
+def a_c():
+    a_v.set(1)
+    b_v.set(0)
+    c_v.set(0)
+    d_v.set(0)
+    e_v.set(0)
+
+def b_c():
+    a_v.set(0)
+    b_v.set(1)
+    c_v.set(0)
+    d_v.set(0)
+    e_v.set(0)
+
+def c_c():
+    a_v.set(0)
+    b_v.set(0)
+    c_v.set(1)
+    d_v.set(0)
+    e_v.set(0)
+
+def d_c():
+    a_v.set(0)
+    b_v.set(0)
+    c_v.set(0)
+    d_v.set(1)
+    e_v.set(0)
+
+def e_c():
+    a_v.set(0)
+    b_v.set(0)
+    c_v.set(0)
+    d_v.set(0)
+    e_v.set(1)
+
+def remplir(cadre_d, couleur):
+    cadre_d.create_polygon(0, 0, 20, 0, 20, 20, 0, 20, fill = couleur)
+
+def load_types():
+    global types
+    f = open('TYPES', 'r')
+    s = f.readlines()
+    f.close()
+    for i in range(5):
+        a, b, c = s[i].split('\n')[0].split()
+        types[i] = (a, b, c)
+    rafraichir()
+
+def update():
+    a = a_l1.get('@0,0', END)
+    b = a_l2.get('@0,0', END)
+    c = a_l3.get('@0,0', END)
+    types[0] = (a, b, c)
+    a = b_l1.get('@0,0', END)
+    b = b_l2.get('@0,0', END)
+    c = b_l3.get('@0,0', END)
+    types[1] = (a, b, c)
+    a = c_l1.get('@0,0', END)
+    b = c_l2.get('@0,0', END)
+    c = c_l3.get('@0,0', END)
+    types[2] = (a, b, c)
+    a = d_l1.get('@0,0', END)
+    b = d_l2.get('@0,0', END)
+    c = d_l3.get('@0,0', END)
+    types[3] = (a, b, c)
+    a = e_l1.get('@0,0', END)
+    b = e_l2.get('@0,0', END)
+    c = e_l3.get('@0,0', END)
+    types[4] = (a, b, c)
 
 # Création de la fenêtre
 gui = Tk()
-gui.geometry(str(larg) + "x" + str(long + 30) + "+50+50")
+gui.geometry(str(larg + 190) + "x" + str(long + 30) + "+50+50")
 gui.title("Prologin 2016 - Map editor")
 
 # Création du Canvas
 cadre = Canvas(gui, width = larg, height = long)
-cadre.grid(columnspan = 7, row = 1)
+cadre.grid(columnspan = 8, row = 1, rowspan = 6)
 cadre.bind("<Button-1>", click_gauche)
-cadre.bind("<Key>", appui_clavier)
 cadre.focus_set()
 
 label_text = Label(gui, text = 'N')
@@ -219,20 +363,107 @@ label_text.grid(row = 0, column = 0)
 text_n = Text(gui, height = 1, width = 5)
 text_n.grid(row = 0,column = 1)
 
-button_gene = Button(gui, text = 'GENERATION', command = gene)
+button_gene = Button(gui, text = 'CLEAR', command = clear)
 button_gene.grid(row = 0, column = 2)
 
+button_gene = Button(gui, text = 'GENERATION', command = gene)
+button_gene.grid(row = 0, column = 3)
+
 label_text = Label(gui, text = 'File')
-label_text.grid(row = 0, column = 3)
+label_text.grid(row = 0, column = 4)
 
 text = Text(gui, height = 1, width = 25)
-text.grid(row = 0,column = 4)
+text.grid(row = 0,column = 5)
 
 button_save = Button(gui, text = 'SAVE', command = save)
-button_save.grid(row = 0, column = 5)
+button_save.grid(row = 0, column = 6)
 
 button_save = Button(gui, text = 'LOAD', command = load)
-button_save.grid(row = 0, column = 6)
+button_save.grid(row = 0, column = 7)
+
+label_l = Label(gui, text = "PERIOD TIME POWER")
+label_l.grid(row = 1, column = 8, columnspan = 5)
+
+a_v = IntVar()
+a = Checkbutton(gui, text='A', variable=a_v, command = a_c)
+a.grid(row = 2, column = 8)
+
+b_v = IntVar()
+b = Checkbutton(gui, text='B', variable=b_v, command = b_c)
+b.grid(row = 3, column = 8)
+
+c_v = IntVar()
+c = Checkbutton(gui, text='C', variable=c_v, command = c_c)
+c.grid(row = 4, column = 8)
+
+d_v = IntVar()
+d = Checkbutton(gui, text='D', variable=d_v, command = d_c)
+d.grid(row = 5, column = 8)
+
+e_v = IntVar()
+e = Checkbutton(gui, text='E', variable=e_v, command = e_c)
+e.grid(row = 6, column = 8)
+
+a_d = Canvas(gui, width = 20, height = 20)
+a_d.grid(column = 9, row = 2)
+remplir(a_d, 'black')
+
+a_d = Canvas(gui, width = 20, height = 20)
+a_d.grid(column = 9, row = 3)
+remplir(a_d, 'red')
+
+a_d = Canvas(gui, width = 20, height = 20)
+a_d.grid(column = 9, row = 4)
+remplir(a_d, 'yellow')
+
+a_d = Canvas(gui, width = 20, height = 20)
+a_d.grid(column = 9, row = 5)
+remplir(a_d, 'blue')
+
+a_d = Canvas(gui, width = 20, height = 20)
+a_d.grid(column = 9, row = 6)
+remplir(a_d, 'green')
+
+a_l1 = Text(gui, width = 5, height = 1)
+a_l1.grid(row = 2, column = 10)
+a_l2 = Text(gui, width = 5, height = 1)
+a_l2.grid(row = 2, column = 11)
+a_l3 = Text(gui, width = 5, height = 1)
+a_l3.grid(row = 2, column = 12)
+
+b_l1 = Text(gui, width = 5, height = 1)
+b_l1.grid(row = 3, column = 10)
+b_l2 = Text(gui, width = 5, height = 1)
+b_l2.grid(row = 3, column = 11)
+b_l3 = Text(gui, width = 5, height = 1)
+b_l3.grid(row = 3, column = 12)
+
+c_l1 = Text(gui, width = 5, height = 1)
+c_l1.grid(row = 4, column = 10)
+c_l2 = Text(gui, width = 5, height = 1)
+c_l2.grid(row = 4, column = 11)
+c_l3 = Text(gui, width = 5, height = 1)
+c_l3.grid(row = 4, column = 12)
+
+d_l1 = Text(gui, width = 5, height = 1)
+d_l1.grid(row = 5, column = 10)
+d_l2 = Text(gui, width = 5, height = 1)
+d_l2.grid(row = 5, column = 11)
+d_l3 = Text(gui, width = 5, height = 1)
+d_l3.grid(row = 5, column = 12)
+
+e_l1 = Text(gui, width = 5, height = 1)
+e_l1.grid(row = 6, column = 10)
+e_l2 = Text(gui, width = 5, height = 1)
+e_l2.grid(row = 6, column = 11)
+e_l3 = Text(gui, width = 5, height = 1)
+e_l3.grid(row = 6, column = 12)
+
+button_types = Button(gui, text = "LOAD TYPES", command = load_types)
+button_types.grid(row = 0, column = 8, columnspan = 3)
+
+button_types = Button(gui, text = "UPDATE", command = update)
+button_types.grid(row = 0, column = 11, columnspan = 2)
 
 start()
 
