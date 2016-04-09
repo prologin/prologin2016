d68110a398237ff2d98bb57931c0a9dfcdbfbae6
Sacha Delanoue <contact@shaac.me>
2016-04-09 19:52:56 +0200

    test: unit tests for Api::adversaire

--- src/tests/test-api.cc
+++ src/tests/test-api.cc
@@ -9,3 +9,12 @@ TEST_F(ApiTest, Api_Moi)
         EXPECT_EQ(player.id, player.api->moi());
     }
 }
+
+TEST_F(ApiTest, Api_Adversaire)
+{
+    for (int player_index : {0, 1})
+    {
+        int expected = players[(player_index + 1) % 2].id;
+        EXPECT_EQ(expected, players[player_index].api->adversaire());
+    }
+}
