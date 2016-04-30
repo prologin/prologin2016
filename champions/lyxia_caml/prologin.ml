959a8385cad7726d8ed8f2585b872d76f9a24b6b
lyxia <li-yao.xia@ens.fr>
2016-04-30 16:48:42 +0200

    champions: update lyxia_caml

--- champions/lyxia_caml/prologin.ml
+++ champions/lyxia_caml/prologin.ml
@@ -15,6 +15,8 @@ let (>>=) x f = x |> List.map f |> List.concat
 
 let neighbors (x, y) = [(x+1, y); (x, y+1); (x, y-1); (x-1, y)]
 
+let active_cells = Hashtbl.create 200
+
 exception Break
 
 let try_move_vacuum () =
@@ -53,24 +55,23 @@ let path_to = Array.make_matrix tt tt []
 
 module PSet = Set.Make (struct type t = int * int let compare = compare end)
 
-let pulsar_neighbors = liste_pulsars () |> Array.to_list |> List.map unp >>= neighbors |> PSet.of_list
+let downstream ((x, y) as p') =
+  neighbors p'
+    |> List.filter
+      (fun (x1, y1) -> None <> ds.(x1).(y1) && ds.(x).(y) > ds.(x1).(y1))
 
 let rec mark tgt ((x, y) as p') =
   match path_to.(x).(y) with
   | tgt' :: _ when tgt = tgt' -> ()
   | tgts ->
       path_to.(x).(y) <- tgt :: tgts;
-      try
-        neighbors p'
-          |> List.filter
-            (fun (x1, y1) -> None <> ds.(x1).(y1) && ds.(x).(y) > ds.(x1).(y1))
-          |> List.iter (mark tgt)
-      with
-      | Not_found -> ()
+      downstream p' |> List.iter (mark tgt)
 
-let _ = pulsar_neighbors |> PSet.iter (fun p' -> mark p' p')
+let _ = liste_pulsars () |> Array.to_list |> List.map unp >>= neighbors |> List.iter (fun p' -> mark p' p')
 
-let pipes_to_build = ref []
+let path_to = Array.map (Array.map List.length) path_to
+
+let to_build = ref None
 
 let rec maximum_fst = function
   | [x] -> x
@@ -79,59 +80,151 @@ let rec maximum_fst = function
       if x1 < x2 then p else q
   | _ -> assert false
 
-let rec flow ((x, y) as p) =
-  let go ps =
-    ps
-      |> List.map (fun ((x1, y1) as p1) ->
-        (List.length path_to.(x1).(y1), p1))
-      |> maximum_fst
-      |> snd
-      |> flow
-  in
-  pipes_to_build := p :: !pipes_to_build;
-  neighbors p
-    |> List.filter (fun (x1, y1) ->
-        None <> ds.(x1).(y1) && ds.(x).(y) > ds.(x1).(y1))
-    |> function
-      | [] -> ()
-      | ps -> go ps
+let downstream' p' k =
+  match downstream p' with
+  | [] -> ()
+  | xs -> xs |> List.map (fun ((x1, y1) as p1) -> path_to.(x1).(y1), p1)
+    |> maximum_fst |> snd |> k
 
-let _ = pulsar_neighbors |> PSet.iter flow
-
-let pipes_to_build = !pipes_to_build
-
-let try_fix p =
-  match type_case (mkp p) with
-  | Debris ->
-      ignore (deblayer (mkp p));
-      raise Break
+let construire' p =
+  match construire p with
+  | Ok -> Hashtbl.add active_cells p ()
   | _ -> ()
+
+let build_from { periode ; nombre_pulsations } ((x, y) as p') =
+  let n = ref 0 in
+  let b = ref (fun () -> ()) in
+  let rec build_from ((x, y) as p') =
+    downstream' p' build_from;
+    let b0 = !b in
+    if est_libre (mkp p') then (
+      n := !n + cout_construction_tuyau;
+      b := fun () -> b0 (); ignore (construire' (mkp p'))
+    ) else if est_debris (mkp p') then (
+      n := !n + cout_deblayage + cout_construction_tuyau;
+      b := fun () -> b0 (); ignore (deblayer (mkp p')); ignore (construire' (mkp p'));
+    );
+  in
+  match ds.(x).(y) with
+  | None -> ()
+  | Some d ->
+      build_from p';
+      let tour_fin = 2 * (!n / nb_points_action) + tour_actuel () in
+      let tour_epuise =
+        let x = tour_actuel () + periode * nombre_pulsations in
+        x - (x mod periode)
+      in
+      let w = ((tour_fin - 1) / periode + 1) * periode in
+      match !to_build with
+      | _ when !n = 0 || nombre_pulsations = 0 || tour_epuise < tour_fin -> ()
+      | Some (v, c) when v < w -> ()
+      | _ -> to_build := Some (w, !b)
+
+let try_fix p () =
+  if est_debris p then ignore (deblayer p);
+  if est_libre p then ignore (construire p)
 
 let try_fix_pipes () =
-  try
-    List.iter try_fix pipes_to_build
-  with
-  | Break -> ()
-
-let try_build p =
-  match construire (mkp p) with
-  | Ok -> raise Break
-  | _ -> ()
+  Hashtbl.iter try_fix active_cells
 
 let try_build_pipes () =
-  while points_action () >= cout_construction_tuyau &&
+  let x = ref (-1) in
+  while points_action () <> !x do
+    x := points_action ();
+    to_build := None;
+    liste_pulsars ()
+      |> Array.iter (fun p ->
+        let info = info_pulsar p in
+        neighbors (unp p) |> List.iter (build_from info));
+    match !to_build with
+    | None -> ()
+    | Some (_, b) -> b ()
+  done
+
+let aspi = Array.make_matrix tt tt (max_int, [])
+
+let reset_aspi () =
+  for i = 1 to tt - 2 do
+    for j = 1 to tt - 2 do
+      aspi.(i).(j) <- (max_int, [])
+    done
+  done
+
+let go p0 p =
+  let k = puissance_aspiration (mkp p0) in
+  let q = Queue.create () in
+  Queue.push (-k, p) q;
+  while not (Queue.is_empty q) do
     try
-      List.iter try_build pipes_to_build;
-      false
+      let d, ((x, y) as p) = Queue.pop q in
+      if est_tuyau (mkp p) || est_super_tuyau (mkp p) then (
+        let (e, s) = aspi.(x).(y) in
+        if e = d then aspi.(x).(y) <- (e, p0 :: s)
+        else if d < e then aspi.(x).(y) <- (d, [p0])
+        else raise Break;
+        neighbors p |> List.iter (fun p -> Queue.push (d+1, p) q))
     with
-    | Break -> true
-  do () done
+    | Break -> ()
+  done
+
+let update_aspi () =
+  reset_aspi ();
+  for i = tt/3 to tt/3 + longueur_base - 1 do
+    go (i,0) (i,1);
+    go (i,tt-1) (i,tt-2);
+    go (0,i) (1,i);
+    go (tt-1,i) (tt-2,i);
+  done
+
+let destroy' (x, y) =
+  if x = 0 then
+    (ignore (detruire (mkp (1, y))); raise Break)
+  else if x = tt-1 then
+    (ignore (detruire (mkp (tt-2, y))); raise Break)
+
+let try_destroy () =
+  let worth p =
+    int_of_float (charges_presentes (mkp p)) > charge_destruction in
+  for i = 1 to tt-2 do
+    [(2, i); (tt-3, i)] |> List.iter (fun ((x, y) as p) ->
+      match aspi.(x).(y) with
+      | _, [p0] when worth p ->
+          destroy' p0
+      | _ -> ())
+  done;
+  for i = 1 to tt-2 do
+    for j = 1 to tt-2 do
+      let _, aa = aspi.(i).(j) in
+      let f (x, _) = x = 1 || x = tt-1 in
+      match aa, List.filter f aa with
+      | _ :: _ :: _, [p0] when worth (i, j) -> destroy' p0
+      | _ -> ()
+    done
+  done
+
+let try_destroy () = try try_destroy () with Break -> ()
+
+let first_path = ref true
+
+let try_double_pipe () =
+  if tour_actuel () > 8 then (
+    if est_tuyau (mkp (tt/2, tt-2)) then
+      for i = tt-2 to tt-6 do
+        construire' (mkp (tt/2-1, i))
+      done;
+    if est_tuyau (mkp (tt/2, 1)) then
+      for i = 1 to 4 do
+        construire' (mkp (tt/2+1, i))
+      done)
 
 let partie_init () = ()
 
 let jouer_tour () =
+  update_aspi ();
   try_move_vacuum ();
+  try_double_pipe ();
   try_fix_pipes ();
+  try_destroy ();
   try_build_pipes ()
 
 let partie_fin () = ();;
