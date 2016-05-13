743c1f3f89beddfc4fab2421179ede24a32000a6
Alexandre Macabies <web+git@zopieux.com>
2016-05-14 01:40:25 +0200

    theme: fix dropbox CSS & table links

--- www/static/js/header.js
+++ www/static/js/header.js
@@ -1,34 +1,14 @@
-/*$(function () {
- let logo = [7, 11, 13, 0, 0, 0, 0, 0, 0, 5, 0, 0, 0, 0, 0, 0, 5, 0, 0, 0, 6, 0, 6, 0, 0, 0, 0, 0, 0, 6, 0, 0, 0, 0, 0, 0, 2, 0, 0, 0, 8, 11, 10, 7, 11, 9, 7, 11, 13, 6, 7, 11, 13, 7, 11, 13, 6, 7, 11, 13, 6, 0, 0, 6, 0, 0, 6, 0, 6, 6, 6, 0, 6, 6, 0, 6, 6, 6, 0, 6, 6, 0, 0, 6, 0, 0, 4, 11, 10, 6, 4, 11, 10, 4, 11, 14, 6, 6, 0, 6, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 6, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 3, 11, 10, 0, 0, 0, 0];
- let lw = 20, lh = 7, ls = .5;
-
- let svg = d3.select('.navbar-brand').append('svg');
- let defs = svg.append('defs');
- for (let i = 0; i < 15; i++) {
- defs
- .append('symbol')
- .attr('id', 'p' + i)
- .append('image')
- .attr('transform', 'translate(-' + i * 32 + ' 0)')
- .attr('xlink:href', '/static/pipes.png')
- .attr('width', 32 * 20)
- .attr('height', 32 * 2);
- }
-
- let group = svg.append('g')
- .attr('transform', 'scale(.33)')
- let tiles = group.selectAll('use').data(logo.map((e, i) => ({
- x: i % lw,
- y: parseInt(i / lw),
- e: e - 1
- })).filter(d => d.e != -1));
- tiles.enter().append('use')
- .attr('x', d => d.x * 32)
- .attr('y', d => d.y * 32)
- .attr('xlink:href', d => '#p' + d.e)
- .attr('width', 32)
- .attr('height', 32);
- });*/
+function shuffle(array) {
+  let counter = array.length;
+  while (counter > 0) {
+    let index = Math.floor(Math.random() * counter);
+    counter--;
+    let temp = array[counter];
+    array[counter] = array[index];
+    array[index] = temp;
+  }
+  return array;
+}
 
 $(function () {
   $('.navbar-brand').load('/static/logo.svg', () => {
@@ -54,12 +34,28 @@ $(function () {
         .transition().duration(1000 + Math.random() * 6000)
         .attr('fill-opacity', 0)
         .attr('transform', 'scale(' + Math.random() * 4 / 7 + ') rotate(' + alpha + ')')
-        .transition().duration(0).each('end', function() { d3.select(this.parentNode).remove(); });
-      console.log('heh');
+        .transition().duration(0).each('end', function () {
+        d3.select(this.parentNode).remove();
+      });
       setTimeout(popStar, 1000 + Math.random() * 10 * 1000);
     }
 
     for (let i = 0; i < 20; i++)
       setTimeout(popStar, Math.random() * 10 * 1000);
+
+    let letters = svg.select('#text4323').attr('style', '')
+      .selectAll('path');
+    let dur = [];
+    for (let i = 0; i < letters.size(); i++)
+      dur.push(i * 200);
+
+    shuffle(dur);
+
+    letters
+      .style('opacity', 0)
+      .transition().duration(1200).delay(function (d, i) {
+      return dur[i];
+    })
+      .style('opacity', 1);
   });
 });
