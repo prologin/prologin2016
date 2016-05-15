5d1bf631794a171429432c0eb64666089377190d
Pierre Bourdon <delroth@gmail.com>
2016-05-15 11:55:24 +0200

    tournoi: add CSS for tournoi_out

--- tournoi/tournoi_out.py
+++ tournoi/tournoi_out.py
@@ -60,6 +60,20 @@ th, td {
     padding-left: 10px;
     padding-right: 10px;
 }
+
+.won {
+    color: green;
+}
+
+td > a {
+    color: inherit;
+}
+.lost {
+    color: white;
+    background-color: grey;
+}
+
+table { border-collapse: collapse; text-align: left; width: 100%; } {font: normal 12px/150% Arial, Helvetica, sans-serif; background: #fff; overflow: hidden; border: 1px solid #8C8C8C; -webkit-border-radius: 3px; -moz-border-radius: 3px; border-radius: 3px; }table td, table th { padding: 3px 10px; }table thead td {background:-webkit-gradient( linear, left top, left bottom, color-stop(0.05, #8C8C8C), color-stop(1, #7D7D7D) );background:-moz-linear-gradient( center top, #8C8C8C 5%, #7D7D7D 100% );filter:progid:DXImageTransform.Microsoft.gradient(startColorstr='#8C8C8C', endColorstr='#7D7D7D');background-color:#8C8C8C; color:#FFFFFF; font-size: 15px; font-weight: bold; border-left: 1px solid #A3A3A3; } table thead td:first-child { border: none; }table tbody td { color: #7D7D7D; border-left: 1px solid #DBDBDB;font-size: 12px;font-weight: normal; }table tbody .alt td { background: #EBEBEB; color: #7D7D7D; }table tbody td:first-child { border-left: none; }table tbody tr:last-child td { border-bottom: none; }table tfoot td div { border-top: 1px solid #8C8C8C;background: #EBEBEB;} table tfoot td { padding: 0; font-size: 12px } table tfoot td div{ padding: 2px; }table tfoot td ul { margin: 0; padding:0; list-style: none; text-align: right; }table tfoot  li { display: inline; }table tfoot li a { text-decoration: none; display: inline-block;  padding: 2px 8px; margin: 1px;color: #F5F5F5;border: 1px solid #8C8C8C;-webkit-border-radius: 3px; -moz-border-radius: 3px; border-radius: 3px; background:-webkit-gradient( linear, left top, left bottom, color-stop(0.05, #8C8C8C), color-stop(1, #7D7D7D) );background:-moz-linear-gradient( center top, #8C8C8C 5%, #7D7D7D 100% );filter:progid:DXImageTransform.Microsoft.gradient(startColorstr='#8C8C8C', endColorstr='#7D7D7D');background-color:#8C8C8C; }table tfoot ul.active, table tfoot ul a:hover { text-decoration: none;border-color: #7D7D7D; color: #F5F5F5; background: none; background-color:#8C8C8C;}div.dhtmlx_window_active, div.dhx_modal_cover_dv { position: fixed !important; }
 </style>''')
 
 print('<h1>{}</h1>'.format(tournoi.name))
@@ -109,7 +123,7 @@ for i, c1 in enumerate(l, 1):
     print('<table>')
     print('<thead>')
     print('<td>VS</td>')
-    for i in range(2):
+    for i in range(8):
         print('<td>Match {}</td>'.format(i + 1))
     print('<td>Total</td>')
     print('</thead>')
