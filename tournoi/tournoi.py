287e65f844a6201f02e87ec6799aed3848fa2449
Pierre Bourdon <delroth@gmail.com>
2016-05-15 06:30:48 +0200

    tournoi: add support for bulk creations

--- tournoi/tournoi.py
+++ tournoi/tournoi.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python
 
+import collections
 import django
 import os
 import sys
@@ -8,7 +9,9 @@ import time
 os.environ['DJANGO_SETTINGS_MODULE'] = 'prologin.concours.settings'
 django.setup()
 
+from django.db import connection
 from django.contrib.auth.models import User
+from django.utils.timezone import now
 from prologin.concours.stechec.models import Tournament, Match, MatchPlayer, Champion, TournamentPlayer, Map
 
 from tournoi_common import get_champions
@@ -20,16 +23,6 @@ prologin = User.objects.get(username="seirl")
 tournoi = Tournament.objects.create()
 
 print('Launching tournament {}'.format(tournoi.id))
-
-def lancer_match(c1, c2, map):
-    m = Match(author=prologin,
-              tournament=tournoi)
-    m.map = map.path
-    m.status = 'new'
-    m.save()
-    MatchPlayer(champion=c1, match=m).save()
-    MatchPlayer(champion=c2, match=m).save()
-    return m.id
 
 chs = get_champions()
 
@@ -46,10 +39,33 @@ for m in maps:
 input()
 print()
 print('Launching matches...')
-for c1 in chs:
-    for c2 in chs:
-        for map in maps:
-           if c1.id == c2.id:
-               continue
-           print(c1.id, '-', c2.id)
-           lancer_match(c1, c2, map)
+match_objs = []
+ts = now()
+for map in maps:
+    for i in range(len(chs) ** 2):
+        m = Match(author=prologin, tournament=tournoi)
+        m.map = map.path
+        m.ts = ts
+        match_objs.append(m)
+Match.objects.bulk_create(match_objs)
+
+matches = Match.objects.filter(ts=ts)
+matches_per_map = collections.defaultdict(list)
+for m in matches:
+    matches_per_map[m.map].append(m)
+
+player_objs = []
+for map in maps:
+    print(map.path)
+    map_matches = matches_per_map[map.path]
+    for c1 in chs:
+        for c2 in chs:
+            m = map_matches.pop()
+            player_objs.append(MatchPlayer(champion=c1, match=m))
+            player_objs.append(MatchPlayer(champion=c2, match=m))
+MatchPlayer.objects.bulk_create(player_objs)
+
+with connection.cursor() as c:
+    c.execute('''
+        UPDATE stechec_match SET status='new' WHERE id IN (%s)
+    ''' % ','.join(str(m.id) for m in matches))
